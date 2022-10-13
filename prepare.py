444d5c96d5baba43a3a664a27cef85541e12b010
Michael Haerle <michael.haerle01@gmail.com>
2022-10-13 11:35:47 -0500

    added exercises

---
+++ prepare.py
@@ -0,0 +1,97 @@
+# from __future__ import division
+import itertools
+
+# To get rid of those blocks of red warnings
+import warnings
+warnings.filterwarnings("ignore")
+
+# Standard Imports
+import numpy as np
+from scipy import stats
+import pandas as pd
+from math import sqrt
+import os
+from scipy.stats import spearmanr
+from sklearn import metrics
+from random import randint
+
+
+# Vis Imports
+import matplotlib.pyplot as plt
+from matplotlib import style
+import seaborn as sns
+import plotly.express as px
+from pandas.plotting import register_matplotlib_converters
+from mpl_toolkits.mplot3d import Axes3D
+
+# Modeling Imports
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression, LassoLars, TweedieRegressor
+from sklearn.metrics import mean_squared_error, r2_score, explained_variance_score
+from sklearn.feature_selection import f_regression 
+from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.feature_selection import SelectKBest, f_regression, RFE
+import sklearn.preprocessing
+import statsmodels.api as sm
+from sklearn.cluster import DBSCAN
+
+# NLP Imports
+import unicodedata
+import re
+import json
+import requests
+from bs4 import BeautifulSoup
+import nltk
+from nltk.tokenize.toktok import ToktokTokenizer
+from nltk.corpus import stopwords
+
+def basic_clean(original):
+    article = original.lower()
+    article = unicodedata.normalize('NFKD', article)\
+    .encode('ascii', 'ignore')\
+    .decode('utf-8')
+    #use re.sub to remove special characters
+    article = re.sub(r'[^a-z0-9\'\s]', '', article)
+    return article
+
+def tokenize(article):
+    #create the tokenizer
+    tokenize = nltk.tokenize.ToktokTokenizer()
+    #use the tokenizer
+    article = tokenize.tokenize(article, return_str=True)
+    return article
+
+def stem(article):
+    #create porter stemmer
+    ps = nltk.porter.PorterStemmer()
+    stems = [ps.stem(word) for word in article.split()]
+    #join words back together
+    article_stemmed = ' '.join(stems)
+    return article_stemmed
+
+def lemmatize(article):
+    #create the lemmatizer
+    wnl = nltk.stem.WordNetLemmatizer()
+    lemmas = [wnl.lemmatize(word) for word in article.split()]
+    #join words back together
+    article_lemmatized = ' '.join(lemmas)
+    return article_lemmatized
+
+def remove_stopwords(article):
+    #save stopwords
+    stopwords_ls = stopwords.words('english')
+    words = article.split()
+    #remove stopwords from list of words
+    filtered_words = [word for word in words if word not in stopwords_ls]
+    #join words back together
+    article = ' '.join(filtered_words)
+    return article
+
+def prepare_article(original):
+    original['clean'] = original['content'].apply(basic_clean).apply(tokenize).apply(remove_stopwords)
+    original['stemmed'] = original['content'].apply(basic_clean).apply(tokenize).apply(remove_stopwords).apply(stem)
+    original['lemmatized'] = original['content'].apply(basic_clean).apply(tokenize).apply(remove_stopwords).apply(lemmatize)
+    original.rename(columns = {'content':'original'}, inplace = True)
+    return original
